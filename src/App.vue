21771f8faa3e1ef56ceeee6ad266f662eb5199f2
Scaaz <szymon.czekaj0@gmail.com>
2024-10-17 19:49:52 +0200

    podświetlanie ruchu dla pionków oraz fix ikonek

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="container">
     <div id="gameboard">
+    <div v-for="(col,colIndex) in chessBoard" :key="colIndex">
+      <div  @click="clicked(colIndex, rowIndex, piece.type, piece.color)"  v-for="(piece,rowIndex) in col" v-bind:class = "(rowIndex%2===0 + colIndex%2)?'light':'dark'" class="square"  :key="rowIndex">   
 
-    <div v-for="(col,colIndex) in chessBoard" class="row" :key="colIndex">
-      <div  style="color:red" v-for="(piece,rowIndex) in col" v-bind:class = "(rowIndex%2===0 + colIndex%2)?'light':'dark'" class="square"  :key="rowIndex">   
-        <font-awesome-icon :icon="chessPieceIcons[piece.type]" size="2x" :color="chessPieceColors[piece.color]" />  
+        <div v-bind:class="(isHighlighted(rowIndex, colIndex)) ? (piece.type === null ? 'move' : 'attack') : ''"></div>
+        <font-awesome-icon v-if="piece.type !=null" :icon="chessPieceIcons[piece.type]" size="2x" :color="chessPieceColors[piece.color]" />  
 
       </div>
     </div>
@@ -15,6 +16,8 @@
 
 
 <script setup>
+import { ref, watch } from 'vue'
+
 let chessPieceIcons = {
   1:'chess-pawn',
   2:'chess-rook',
@@ -30,15 +33,73 @@ let chessPieceColors = {
 }
 
 let chessBoard = [
-    [{type: 2, color: 0}, {type: 1, color: 0}, {icon: null, color: null}, {icon: null, color: null}, {icon: null, color: null}, {icon: null, color: null}, {type: 1, color: 1}, {type: 2, color: 1}],
-    [{type: 3, color: 0}, {type: 1, color: 0}, {icon: null, color: null}, {icon: null, color: null}, {icon: null, color: null}, {icon: null, color: null}, {type: 1, color: 1}, {type: 3, color: 1}],
-    [{type: 4, color: 0}, {type: 1, color: 0}, {icon: null, color: null}, {icon: null, color: null}, {icon: null, color: null}, {icon: null, color: null}, {type: 1, color: 1}, {type: 4, color: 1}],
-    [{type: 5, color: 0}, {type: 1, color: 0}, {icon: null, color: null}, {icon: null, color: null}, {icon: null, color: null}, {icon: null, color: null}, {type: 1, color: 1}, {type: 6, color: 1}],
-    [{type: 6, color: 0}, {type: 1, color: 0}, {icon: null, color: null}, {icon: null, color: null}, {icon: null, color: null}, {icon: null, color: null}, {type: 1, color: 1}, {type: 5, color: 1}],
-    [{type: 4, color: 0}, {type: 1, color: 0}, {icon: null, color: null}, {icon: null, color: null}, {icon: null, color: null}, {icon: null, color: null}, {type: 1, color: 1}, {type: 4, color: 1}],
-    [{type: 3, color: 0}, {type: 1, color: 0}, {icon: null, color: null}, {icon: null, color: null}, {icon: null, color: null}, {icon: null, color: null}, {type: 1, color: 1}, {type: 3, color: 1}],
-    [{type: 2, color: 0}, {type: 1, color: 0}, {icon: null, color: null}, {icon: null, color: null}, {icon: null, color: null}, {icon: null, color: null}, {type: 1, color: 1}, {type: 2, color: 1}]
+    [{type: 2, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 2, color: 1}],
+    [{type: 3, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 3, color: 1}],
+    [{type: 4, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 4, color: 1}],
+    [{type: 5, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 6, color: 1}],
+    [{type: 6, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 5, color: 1}],
+    [{type: 4, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 4, color: 1}],
+    [{type: 3, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 3, color: 1}],
+    [{type: 2, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 2, color: 1}]
 ];
+
+
+const coordinatesSet = ref(new Set());
+watch(coordinatesSet)
+
+
+function isHighlighted(rowIndex, colIndex) {
+  return coordinatesSet.value.has(`${rowIndex},${colIndex}`);
+}
+
+function clicked(colIndex, rowIndex, pieceType, pieceColor)
+{
+  coordinatesSet.value.clear();
+  switch(pieceType)
+  {
+    case null:
+      
+      break;
+    case 1:
+      for(let i = 1 ; i <=2; i++)
+      {
+        let currentRow;
+        if(pieceColor)
+        {
+          currentRow = rowIndex -i;          
+        }
+        else
+        {
+          currentRow = rowIndex +i; 
+        }
+        coordinatesSet.value.add((currentRow) + ',' + colIndex);
+        if(chessBoard[colIndex][currentRow].type!=null)
+        {
+          console.log("row=" + currentRow + " col="+ colIndex)
+          console.log(chessBoard[currentRow][colIndex])
+          break;
+        }
+      }
+
+      break;
+      case 2:
+      break;
+
+    case 3:
+      break;
+
+      case 4:
+      break;
+
+    case 5:
+      break;
+
+    case 6:
+      break;
+  }
+  
+}
+
 
 </script>
 <style scoped>
@@ -58,6 +119,27 @@ let chessBoard = [
   width: 40px;
 }
 
+.move {
+  background-color: gray;
+  border-radius: 50%;
+  height: 15px;
+  width: 15px;
+  opacity: 50%;
+  position: absolute;
+}
+
+
+.attack {
+  background-color: transparent;
+  border: 4px solid gray; 
+  border-radius: 50%; 
+  height: 40px; 
+  width: 40px; 
+  opacity: 50%; 
+  position: absolute
+}
+
+
 .whitePiece{
   color: white;
 }
@@ -67,11 +149,11 @@ let chessBoard = [
 }
 
 .light{
-  background-color: beige;
+  background-color: #FFD599;
 }
 
 .dark{
-  background-color: gray;
+  background-color: #B16E41;
 }
 
 .container {
