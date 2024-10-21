db5cdec1e025631a850ce93eca4dee3dec84b3f7
Scaaz <szymon.czekaj0@gmail.com>
2024-10-21 17:12:47 +0200

    finished moves for other figures

--- src/App.vue
+++ src/App.vue
@@ -1,149 +1,196 @@
 <template>
   <div class="container">
     <div id="gameboard">
-    <div v-for="(col,colIndex) in chessBoard" :key="colIndex">
-      <div  @click="clicked(colIndex, rowIndex, piece.type, piece.color)"  v-for="(piece,rowIndex) in col" v-bind:class = "(rowIndex%2===0 + colIndex%2)?'light':'dark'" class="square"  :key="rowIndex">   
-
-        <div v-bind:class="(isHighlighted(rowIndex, colIndex)) ? (piece.type === null ? 'move' : 'attack') : ''"></div>
-        <font-awesome-icon v-if="piece.type !=null" :icon="chessPieceIcons[piece.type]" size="2x" :color="chessPieceColors[piece.color]" />  
-
+      <div v-for="(col, colIndex) in chessBoard" :key="colIndex">
+        <div
+          @click="clicked(colIndex, rowIndex, piece.type, piece.color)"
+          v-for="(piece, rowIndex) in col"
+          v-bind:class="rowIndex % 2 === 0 + (colIndex % 2) ? 'light' : 'dark'"
+          class="square"
+          :key="rowIndex"
+        >
+          <div
+            v-bind:class="
+              isHighlighted(rowIndex, colIndex)
+                ? piece.type === null
+                  ? 'move'
+                  : 'attack'
+                : ''
+            "
+          ></div>
+          <font-awesome-icon
+            v-if="piece.type != null"
+            :icon="chessPieceIcons[piece.type]"
+            size="2x"
+            :color="chessPieceColors[piece.color]"
+          />
+        </div>
       </div>
-    </div>
     </div>
   </div>
 </template>
-
-
 
 <script setup>
 import { ref, watch } from 'vue'
 
 let chessPieceIcons = {
-  1:'chess-pawn',
-  2:'chess-rook',
-  3:'chess-knight',
-  4:'chess-bishop',
-  5:'chess-queen',
-  6:'chess-king'
-};
+  1: 'chess-pawn',
+  2: 'chess-rook',
+  3: 'chess-knight',
+  4: 'chess-bishop',
+  5: 'chess-queen',
+  6: 'chess-king',
+}
 
 let chessPieceColors = {
-0: 'white',
-1: 'black'
+  0: 'white',
+  1: 'black',
 }
 
 let chessBoard = [
-    [{type: 2, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 2, color: 1}],
-    [{type: 3, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 3, color: 1}],
-    [{type: 4, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 4, color: 1}],
-    [{type: 5, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: 2, color: 1}, {type: null, color: null}, {type: 1, color: 1}, {type: 6, color: 1}],
-    [{type: 6, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 5, color: 1}],
-    [{type: 4, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 4, color: 1}],
-    [{type: 3, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 3, color: 1}],
-    [{type: 2, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 2, color: 1}]
+  [{type: 2, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 2, color: 1}],
+  [{type: 3, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 3, color: 1}],
+  [{type: 4, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 4, color: 1}],
+  [{type: 5, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 6, color: 1}],
+  [{type: 6, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 5, color: 1}],
+  [{type: 4, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 4, color: 1}],
+  [{type: 3, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 3, color: 1}],
+  [{type: 2, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 2, color: 1}]
 ];
 
-
-const highlightSet = ref(new Set());
+const highlightSet = ref(new Set())
 watch(highlightSet)
 
-
 function isHighlighted(rowIndex, colIndex) {
-  return highlightSet.value.has(`${colIndex},${rowIndex}`);
+  return highlightSet.value.has(`${colIndex},${rowIndex}`)   
 }
 
-function clicked(colIndex, rowIndex, pieceType, pieceColor)
-{
-  highlightSet.value.clear();
-  switch(pieceType)
-  {
+function moveFigure(rowIndex, colIndex, move, pieceColor, iterator = 1) {
+  let newRow = rowIndex + move.row * iterator
+  let newCol = colIndex + move.col * iterator
+
+  if (newRow < 0 || newRow > 7 || newCol < 0 || newCol > 7) return true // outside board
+  if (chessBoard[newCol][newRow].color === pieceColor) return true // same color piece
+
+  highlightSet.value.add(newCol + ',' + newRow)
+  if (chessBoard[newCol][newRow].type != null) return true // stop if piece found
+}
+
+function clicked(colIndex, rowIndex, pieceType, pieceColor) {
+  highlightSet.value.clear()
+  switch (pieceType) {
     case null:
-      
-      break;
+      break
     case 1:
       //pawn
-      for(let i = 1 ; i <=2; i++)
-      {
-        let newRow = pieceColor ? rowIndex - i : rowIndex + i;
-        if (newRow > 7 || newRow < 0 || chessBoard[colIndex][newRow].color === pieceColor) break;
-        highlightSet.value.add((colIndex) + ',' + newRow);
-        if(chessBoard[colIndex][newRow].type!=null){
-          break;
+      for (let i = 1; i <= 2; i++) {
+        let newRow = pieceColor ? rowIndex - i : rowIndex + i
+        if (
+          newRow > 7 ||
+          newRow < 0 ||
+          chessBoard[colIndex][newRow].color === pieceColor
+        )
+          break
+        highlightSet.value.add(colIndex + ',' + newRow)
+        if (chessBoard[colIndex][newRow].type != null) {
+          break
         }
       }
-      break;
+      break
 
-      case 2:
+    case 2:
       //rook
-      //down
-      for (let i = 1; i < 8; i++) {
-        let newRow = rowIndex + i;
-        if (newRow > 7 || chessBoard[colIndex][newRow].color === pieceColor) break;
-        highlightSet.value.add(colIndex + ',' + newRow);
-        if (chessBoard[colIndex][newRow].type != null) break;
+
+      for (let move of [
+        { row: 1, col: 0 }, // down
+        { row: -1, col: 0 }, // up
+        { row: 0, col: 1 }, // right
+        { row: 0, col: -1 }, // left
+      ]) {
+        for (let i = 1; i < 8; i++) {
+          if (moveFigure(rowIndex, colIndex, move, pieceColor, i)) break
+        }
       }
-
-      //up
-      for (let i = 1; i < 8; i++) {
-        let newRow = rowIndex - i;
-        if (newRow < 0 || chessBoard[colIndex][newRow].color === pieceColor) break;
-        highlightSet.value.add(colIndex + ',' + newRow);
-        if (chessBoard[colIndex][newRow].type != null) break;
-      }
-
-      //right
-      for (let i = 1; i < 8; i++) {
-        let newCol = colIndex + i;
-        if (newCol > 7 || chessBoard[newCol][rowIndex].color === pieceColor) break;
-        highlightSet.value.add(newCol + ',' + rowIndex);
-        if (chessBoard[newCol][rowIndex].type != null) break;
-      }
-
-      //left
-      for (let i = 1; i < 8; i++) {
-        let newCol = colIndex - i;
-        if (newCol < 0 || chessBoard[newCol][rowIndex].color === pieceColor) break;
-        highlightSet.value.add(newCol + ',' + rowIndex);
-        if (chessBoard[newCol][rowIndex].type != null) break;
-      }
-  break;
-
-
+      break
 
     case 3:
-    for(let i = 1 ; i <=3; i++)
-      {
-
-        highlightSet.value.add((rowIndex+2) + ',' + colIndex+1);
-        highlightSet.value.add((rowIndex+2) + ',' + colIndex-1);
-     
+      // knight
+      for (let move of [
+        { row: -2, col: -1 },
+        { row: -2, col: 1 }, // Two up, one left/right
+        { row: -1, col: -2 },
+        { row: -1, col: 2 }, // One up, two left/right
+        { row: 1, col: -2 },
+        { row: 1, col: 2 }, // One down, two left/right
+        { row: 2, col: -1 },
+        { row: 2, col: 1 }, // Two down, one left/right
+      ]) {
+        moveFigure(rowIndex, colIndex, move, pieceColor)
       }
-      break;
+      break
 
-      case 4:
-      break;
+    case 4:
+      //bishop
+      for (let move of [
+        { row: 1, col: 1 }, // bottom-right diagonal
+        { row: 1, col: -1 }, // bottom-left diagonal
+        { row: -1, col: 1 }, // top-right diagonal
+        { row: -1, col: -1 }, // top-left diagonal
+      ]) {
+        for (let i = 1; i < 8; i++) {
+          if (moveFigure(rowIndex, colIndex, move, pieceColor, i)) break
+        }
+      }
+
+      break
 
     case 5:
-      break;
+      // queen
+      for (let move of [
+        { row: 1, col: 0 }, // down
+        { row: -1, col: 0 }, // up
+        { row: 0, col: 1 }, // right
+        { row: 0, col: -1 }, // left
+        { row: 1, col: 1 }, // bottom-right diagonal
+        { row: 1, col: -1 }, // bottom-left diagonal
+        { row: -1, col: 1 }, // top-right diagonal
+        { row: -1, col: -1 }, // top-left diagonal
+      ]) {
+        for (let i = 1; i < 8; i++) {
+          if (moveFigure(rowIndex, colIndex, move, pieceColor, i)) break
+        }
+      }
+
+      break
 
     case 6:
-      break;
+      // king
+      for (let move of [
+        { row: 1, col: 0 }, // down
+        { row: -1, col: 0 }, // up
+        { row: 0, col: 1 }, // right
+        { row: 0, col: -1 }, // left
+        { row: 1, col: 1 }, // bottom-right diagonal
+        { row: 1, col: -1 }, // bottom-left diagonal
+        { row: -1, col: 1 }, // top-right diagonal
+        { row: -1, col: -1 }, // top-left diagonal
+      ]) {
+        moveFigure(rowIndex, colIndex, move, pieceColor)
+      }
+      break
   }
-  
 }
-
-
 </script>
 <style scoped>
-#gameboard{
+#gameboard {
   width: 320px;
   height: 320px;
   background-color: red;
-  display:flex;
+  display: flex;
   flex-wrap: wrap;
 }
 
-.square{
+.square {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -160,32 +207,30 @@ function clicked(colIndex, rowIndex, pieceType, pieceColor)
   position: absolute;
 }
 
-
 .attack {
   background-color: transparent;
-  border: 4px solid gray; 
-  border-radius: 50%; 
-  height: 40px; 
-  width: 40px; 
-  opacity: 50%; 
-  position: absolute
+  border: 4px solid gray;
+  border-radius: 50%;
+  height: 40px;
+  width: 40px;
+  opacity: 50%;
+  position: absolute;
 }
 
-
-.whitePiece{
+.whitePiece {
   color: white;
 }
 
-.blackPiece{
-  color:black
+.blackPiece {
+  color: black;
 }
 
-.light{
-  background-color: #FFD599;
+.light {
+  background-color: #ffd599;
 }
 
-.dark{
-  background-color: #B16E41;
+.dark {
+  background-color: #b16e41;
 }
 
 .container {
