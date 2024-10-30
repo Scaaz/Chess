500b84d085dbffedf9faaad9606e1b792fc4e7dc
Scaaz <szymon.czekaj0@gmail.com>
2024-10-30 22:15:31 +0100

    added castle highlighting

--- src/App.vue
+++ src/App.vue
@@ -51,12 +51,12 @@ let chessPieceColors = {
 
 let chessBoard = [
   [{type: 2, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 2, color: 1}],
-  [{type: 3, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 3, color: 1}],
-  [{type: 4, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 4, color: 1}],
-  [{type: 5, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 6, color: 1}],
+  [{type: null, color: null}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 3, color: 1}],
+  [{type: null, color: null}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 4, color: 1}],
+  [{type: null, color: null}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 6, color: 1}],
   [{type: 6, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 5, color: 1}],
-  [{type: 4, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 4, color: 1}],
-  [{type: 3, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 3, color: 1}],
+  [{type: null, color: null}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 4, color: 1}],
+  [{type: null, color: null}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 3, color: 1}],
   [{type: 2, color: 0}, {type: 1, color: 0}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: null, color: null}, {type: 1, color: 1}, {type: 2, color: 1}]
 ];
 
@@ -68,6 +68,14 @@ watchEffect(currentlySelected)
 
 const highlightedArray = ref([]);
 watchEffect(highlightedArray)
+
+const wasBlackKingMoved = ref(false);
+const wasBlackShortMoved = ref(false);
+const wasBlackLongMoved = ref(false);
+
+const wasWhiteKingMoved = ref(false);
+const wasWhiteShortMoved = ref(false);
+const wasWhiteLongMoved = ref(false);
 
 const currentPlayerColor = ref('white')
 
@@ -93,7 +101,7 @@ function isHighlighted(colIndex,rowIndex) {
   );
 }
 
-  function highlightMoveSquares(colIndex, rowIndex, pieceType, pieceColor) {
+  function GetMoveSquares(colIndex, rowIndex, pieceType, pieceColor) {
     let array = [];    
     let iterations = 1;
     let pieceMoves = [];
@@ -250,69 +258,211 @@ const KingMoves = [
         { row: -1, col: -1 }, // top-left diagonal
       ]
 
-function tileClicked(colIndex, rowIndex, pieceType, pieceColor) {
-   
+function tileClicked(colIndex, rowIndex, pieceType, pieceColor) {   
    if( chessPieceColors[pieceColor] == currentPlayerColor.value){
-      highlightedArray.value = highlightMoveSquares(colIndex, rowIndex, pieceType, pieceColor)
+      highlightedArray.value = GetMoveSquares(colIndex, rowIndex, pieceType, pieceColor)
+
+      if(pieceType == 6)
+      {      
+      if(pieceColor === 1)
+        {
+          if(canCastleLongSide()) {
+            highlightedArray.value.push({rowIndex : 7, colIndex : 1})
+          }
+          if(canCastleShortSide()){
+            highlightedArray.value.push({rowIndex : 7, colIndex : 5})
+          }
+        }
+        else {
+          if(canCastleLongSide()) {
+            highlightedArray.value.push({colIndex : 2, rowIndex : 0})
+          }
+          if(canCastleShortSide()) {           
+            highlightedArray.value.push({colIndex : 6, rowIndex : 0})
+          }
+        }
+      }
+
       currentlySelected.value = {colIndex, rowIndex}
    }
    else if (isHighlighted(colIndex, rowIndex)) {
    
      movePiece(colIndex, rowIndex);
+     if(pieceType == 6)
+     {
+      if(currentPlayerColor.value === chessPieceColors[1])
+      {
+        wasBlackKingMoved.value = true
+      }
+      else
+      {
+        wasWhiteKingMoved.value = true
+      }
+     }
+
+     //TODO: nie zawsze wykrywa że piece type 2 ruszone
+     if(pieceType == 2)
+     {
+      if(currentPlayerColor.value === chessPieceColors[1])
+      {
+        if(currentlySelected.value.col === 0 && currentlySelected.value.row === 7)
+        {
+          wasBlackShortMoved.value = true;
+        }
+        else if(currentlySelected.value.col === 7 && currentlySelected.value.row === 7)
+        {
+          wasBlackLongMoved.value = true;
+        }
+      }
+      else if(currentPlayerColor.value === chessPieceColors[0])
+      {
+        if(currentlySelected.value.col === 0 && currentlySelected.value.row === 7)
+        {
+          wasWhiteShortMoved.value = true;
+        }
+        else if(currentlySelected.value.col === 7 && currentlySelected.value.row === 7)
+        {
+          wasWhiteLongMoved.value = true;
+        }
+      }
+     }
      highlightedArray.value = [];
      isKingChecked()
    }
  }
 
-function isKingChecked() 
+function canCastleLongSide()
 {
-  
-  let kingPosition = null;    
+  if(currentPlayerColor.value === chessPieceColors[0])
+  {
+    //white
+    if(wasWhiteKingMoved.value || wasWhiteLongMoved.value)
+    {
+      return false
+    }
+
+    if(chessBoard[3][0].type !== null || chessBoard[2][0].type !== null || chessBoard[1][0].type !== null)
+    {
+      //figures on way
+      return false;
+    }
+    if(isFieldChecked(3,0) || isFieldChecked(2,0) || isFieldChecked(1,0))
+    {
+      //check on way
+      return false;
+    }
+  }
+  else if (currentPlayerColor.value === chessPieceColors[1])
+  {
+    if(wasBlackKingMoved.value || wasBlackLongMoved.value)
+    {
+      return false
+    }
+
+    if(chessBoard[4][7].type !== null || chessBoard[5][7].type !== null || chessBoard[6][7].type !== null)
+    {
+      //figures on way
+      return false;
+    }
+    if(isFieldChecked(4,7) || isFieldChecked(5,7) || isFieldChecked(6,7))
+    {
+      //check on way
+      return false;
+    }
+  }
+  return true;
+}
+
+function canCastleShortSide()
+{
+  if(currentPlayerColor.value === chessPieceColors[0])
+  {
+   //White
+   if(wasWhiteKingMoved.value || wasWhiteShortMoved.value)
+    {
+      return false
+    }
+
+    //short side
+    if(chessBoard[5][0].type !== null || chessBoard[6][0].type !== null)
+    {
+      //figures on way
+      return false;
+    }
+    if(isFieldChecked(5,0) || isFieldChecked(6,0))
+    {
+      //check on way
+      return false;
+    }  
+  }
+  else if (currentPlayerColor.value === chessPieceColors[1])
+  {
+    //Black
+    if(wasBlackKingMoved.value || wasBlackShortMoved.value)
+    {
+      return false
+    }
+
+    //short side
+    if(chessBoard[2][7].type !== null || chessBoard[1][7].type !== null)
+    {
+      //figures on way
+      return false;
+    }
+    if(isFieldChecked(2,7) || isFieldChecked(1,7))
+    {
+      //check on way
+      return false;
+    }
+  }
+  return true;
+}
+
+function getKingPosition(playerColor)
+{    
   for(let colIndex in chessBoard){
     for(let rowIndex in chessBoard){
       let piece = chessBoard[colIndex][rowIndex];
-    if (piece.type === 6 && chessPieceColors[piece.color] === currentPlayerColor.value) {
-      kingPosition = {colIndex, rowIndex}
-               
+    if (piece.type === 6 && chessPieceColors[piece.color] === playerColor) {
+      return  {colIndex, rowIndex}               
       }
     }
   } 
-
-    if (!kingPosition) {
-        console.log("No king found for the current player.");
-        return false;
-    }
-
-    // Sprawdź, czy którakolwiek figura przeciwnika może zagrażać królowi
-    for (let colIndex = 0; colIndex < chessBoard.length; colIndex++) {
-        for (let rowIndex = 0; rowIndex < chessBoard[colIndex].length; rowIndex++) {
-            let piece = chessBoard[colIndex][rowIndex];
-            
-            // Jeśli to figura przeciwnika
-          if(piece.type != null){           
-            if (chessPieceColors[piece.color] !== currentPlayerColor.value){
-              let attackMoves = highlightMoveSquares(colIndex, rowIndex, piece.type, piece.color); // Funkcja, która zwraca dozwolone ruchy dla figury            
-   
-                // Sprawdź, czy jakikolwiek ruch figury przeciwnika prowadzi na pozycję króla
-                for (let move of attackMoves) {     
-                  if (move.colIndex == kingPosition.colIndex && move.rowIndex == kingPosition.rowIndex) {    
-                    // console.log("i am row="+colIndex+ "col="+rowIndex +", type="+piece.type)
-                    // console.log("i attack" + attackMoves)
-                    //   console.log("king row="+kingPosition.row+"col="+kingPosition.colIndex )
-                    console.log("Check detected on " + currentPlayerColor.value);
-                    currentlyChecked.value = {row : kingPosition.rowIndex, col : kingPosition.colIndex}
-                      return true; // Znaleziono szach
-                    }
-                }
-            }
-          }
-        }
-    }
-
-    console.log("No check.");
-    return false; // Nie ma szacha
 }
 
+function isKingChecked() 
+{  
+  let kingPosition = getKingPosition(currentPlayerColor.value);
+  if (!kingPosition) {
+      console.log("No king found for the current player.");
+      return false;
+  }
+  let isKingChecked = isFieldChecked(kingPosition.colIndex, kingPosition.rowIndex)
+  if(isKingChecked)
+  {
+    currentlyChecked.value = {row : kingPosition.rowIndex, col : kingPosition.colIndex}
+    return true
+  }
+}
+
+function isFieldChecked(checkedColIndex, checkedRowIndex) 
+{
+  for (let colIndex = 0; colIndex < chessBoard.length; colIndex++) {
+    for (let rowIndex = 0; rowIndex < chessBoard[colIndex].length; rowIndex++) {
+      let piece = chessBoard[colIndex][rowIndex];
+      // Jeśli to figura przeciwnika
+      if(piece.type != null && chessPieceColors[piece.color] !== currentPlayerColor.value){  
+        let attackMoves = GetMoveSquares(colIndex, rowIndex, piece.type, piece.color);    
+        for (let move of attackMoves) {     
+          if (move.colIndex == checkedColIndex && move.rowIndex == checkedRowIndex) {                        
+            return true;
+          }
+        }            
+      }
+    }
+  }
+  return false; // Nie ma szacha
+}
 
 
 </script>
